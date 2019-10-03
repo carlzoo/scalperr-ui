907546d9867fff722f81a3245a25ebb2fd604c82
carlzoo <carl.j.zhou@protonmail.com>
2019-10-03 17:48:48 -0400

    move ticketmaster class to components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SideBarMenu from './components/SideBarMenu';
 import Wrapper from './components/Wrapper';
-import Ticketmaster from './Ticketmaster';
+import Ticketmaster from './components/Ticketmaster';
 
 
 const App = () => (
